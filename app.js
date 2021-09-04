1a853411d06e4408165f05fed02c94774a7a23d1
Änubhab <hota000anubhab@gmail.com>
2021-09-04 22:30:30 +0530

    Web RTC

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ const jwt = require('jsonwebtoken');
 const request = require('request');
 var favicon = require('serve-favicon');
 
+
 //app
 const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 //Mongo connection and listen
 const dbURI = env.MONGO_URI;
@@ -21,7 +24,10 @@ mongoose.connect(dbURI, {useNewUrlParser:true,useUnifiedTopology:true})
     .then((result)=> {
         console.log('Connected to DB');
         //listening
-        app.listen(process.env.PORT || 3000);
+        port = process.env.PORT || 3000;
+        server.listen(port, () => {
+            console.log(`Express server listening on port ${port}`)
+          })
     })
     .catch((err) => console.log(err));
 
@@ -39,6 +45,9 @@ app.use(morgan('dev'));
 
 app.get('/',(req,res)=>{
     res.render('index')
+})
+app.get('/call',(req,res)=>{
+  res.render('call')
 })
 app.get('/patientsignup',(req,res)=>{
     res.render('patientsignup')
@@ -66,3 +75,42 @@ app.post('/doctorsignup',(req,res)=>{
              console.log(err);
         });
 })
+
+io.on('connection', (socket) => {
+    socket.on('join', (roomId) => {
+      const selectedRoom = io.sockets.adapter.rooms[roomId]
+      const numberOfClients = selectedRoom ? selectedRoom.length : 0
+  
+      // These events are emitted only to the sender socket.
+      if (numberOfClients == 0) {
+        console.log(`Creating room ${roomId} and emitting room_created socket event`)
+        socket.join(roomId)
+        socket.emit('room_created', roomId)
+      } else if (numberOfClients == 1) {
+        console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+        socket.join(roomId)
+        socket.emit('room_joined', roomId)
+      } else {
+        console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+        socket.emit('full_room', roomId)
+      }
+    })
+  
+    // These events are emitted to all the sockets connected to the same room except the sender.
+    socket.on('start_call', (roomId) => {
+      console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+      socket.broadcast.to(roomId).emit('start_call')
+    })
+    socket.on('webrtc_offer', (event) => {
+      console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+      socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
+    })
+    socket.on('webrtc_answer', (event) => {
+      console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+      socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
+    })
+    socket.on('webrtc_ice_candidate', (event) => {
+      console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+      socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
+    })
+  })
